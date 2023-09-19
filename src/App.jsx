9768d64dff329dd79e1913bcca17fc2f24423c21
Kevin Thomas <kevinthomas0420@gmail.com>
2023-09-19 10:58:08 +0530

    update header

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,30 @@ import "react-toastify/dist/ReactToastify.css";
 import Header from "./Header";
 import OrgHome from "./OrgHome";
 
-function App(props)  {
- 
+function App(props) {
+  const [loginIsShown, setLoginIsShown] = useState(false);
+
+  const showLoginHandler = () => {
+    setLoginIsShown(true);
+  };
+
+  const hideLoginHandler = () => {
+    setLoginIsShown(false);
+  };
 
   return (
     <>
-    
-    
       <Router>
-     
+        {loginIsShown && <Login onClose={hideLoginHandler} {...props} />}
+        <Header onShowLogin={showLoginHandler} />
         <Routes>
-          <Route
-            path="/"
-            element={<HomePage  />}
-          ></Route>
+          <Route path="/" element={<HomePage />}></Route>
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
-          <Route path="/create-tournament" element={<CreateTournament />} />
+          <Route path="/create-tournament" element={<PrivateRoute />}>
+            <Route path="/create-tournament" element={<CreateTournament />} />
+          </Route>
           <Route path="/Organization" element={<OrgHome />} />
         </Routes>
       </Router>
