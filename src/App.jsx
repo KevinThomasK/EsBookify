bd65e8db313e706cbf9a67b7b8adea3b2e9b87da
RahulK2396 <rahulvidx0696@gmail.com>
2023-09-20 11:07:08 +0530

    TournamentList

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App(props)  {
           <Route path="create-tournament" element={<PrivateRoute/>}>
           <Route path="/create-tournament" element={<CreateTournament />} />
           </Route>
-          <Route path="/Organization" element={<OrgHome />} />
+          <Route path="/Organization" element={<OrgHome/>}/>
         </Routes>
       </Router>
 
