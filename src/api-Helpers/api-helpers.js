ade69fc4bd8db18c9451a83239cccb490b35b0b3
Avinash R <avinashradha24@gmail.com>
2023-09-20 15:48:56 +0530

    updated one

--- src/api-Helpers/api-helpers.js
+++ src/api-Helpers/api-helpers.js
@@ -1,19 +1,20 @@
+// api-helpers.js
 import axios from "axios";
 
-export const newTournament  = async (data) => {
-    console.log(data);
-    const res = await axios.post("http://localhost:4000/tournaments/createtournament/",{
-        name:data.tournamentName,
-        dateOfMatch:data.tournamentDate,
-        idpTime:data.tournamentTime,
-        prizePool:data.prizePool,
-        rules:data.rules,
-    }).catch((err)=>console.log(err))
-  
-    if(res.status !== 201){
-      return console.log("unexpected error")
-    }
-  
-    const resData = await res.data;
-    return resData;
+export const newTournament = async (data) => {
+  console.log(data);
+  const res = await axios.post("http://localhost:4000/tournaments/createtournament/", {
+    name: data.tournamentName,
+    dateOfMatch: data.tournamentDate, // Assuming the server accepts the date in the given format
+    idpTime: data.tournamentTime,
+    prizePool: parseInt(data.prizePool), // Convert to integer
+    rules: data.rules,
+  }).catch((err) => console.log(err));
+
+  if (res.status !== 201) {
+    return console.log("unexpected error");
   }
+
+  const resData = await res.data;
+  return resData;
+};
--- src/pages/CreateTournament.jsx
+++ src/pages/CreateTournament.jsx
@@ -8,14 +8,11 @@ import { newTournament } from "../api-Helpers/api-helpers";
 export default function CreateTournament() {
   const [formData, setFormData] = useState({
     tournamentName: "",
-    tournamentDate: "",
+    tournamentDate: "", // Keep the date field as a string
     tournamentTime: "",
     prizePool: "",
     rules: "",
   });
-
-  // const { tournamentName, tournamentDate, tournamentTime, prizePool, rules } =
-  //   formData;
 
   const handleChange = (e) => {
     setFormData((prevState) => ({
@@ -27,9 +24,54 @@ export default function CreateTournament() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(formData);
-    newTournament({ ...formData })
+
+    // Ensure that the "tournamentTime" field is in the 24-hour format (HH:mm)
+    const timePattern = /^([01]\d|2[0-3]):([0-5]\d)$/;
+    if (!timePattern.test(formData.tournamentTime)) {
+      alert("Invalid time format. Please use HH:mm format (24-hour).");
+      return;
+    }
+
+    // Format the date in "dd-mm-yyyy" before sending it to the server
+    const formattedDate = formatDate(formData.tournamentDate);
+
+    // Format the time in "hh:mm AM/PM" before sending it to the server
+    const formattedTime = formatTime(formData.tournamentTime);
+
+    // Send the formatted data to the newTournament function
+    newTournament({ ...formData, tournamentDate: formattedDate, tournamentTime: formattedTime })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
+  };
+
+  // Function to format the date as "dd-mm-yyyy"
+  const formatDate = (date) => {
+    const parts = date.split("-");
+    if (parts.length === 3) {
+      const [year, month, day] = parts;
+      return `${day}-${month}-${year}`;
+    }
+    return date; // Return as-is if not in the expected format
+  };
+
+  // Function to format the time as "hh:mm AM/PM"
+  const formatTime = (time) => {
+    const parts = time.split(":");
+    if (parts.length === 2) {
+      const [hour, minute] = parts;
+      let period = "AM";
+      let formattedHour = parseInt(hour, 10);
+
+      if (formattedHour >= 12) {
+        period = "PM";
+        if (formattedHour > 12) {
+          formattedHour -= 12;
+        }
+      }
+
+      return `${formattedHour}:${minute} ${period}`;
+    }
+    return time; // Return as-is if not in the expected format
   };
 
   return (
@@ -38,7 +80,7 @@ export default function CreateTournament() {
         <img
           className="w-[95%] mx-auto pb-10 pt-10"
           src={createTournamentPageImg}
-          alt="backgorund"
+          alt="background"
         />
         <h2 className="text-white absolute top-[45%] left-[40%] text-4xl font-bold">
           Create <span className="text-[#ff8a01]">Tournament</span>
@@ -58,7 +100,7 @@ export default function CreateTournament() {
             type="text"
             name="tournamentName"
             value={formData.tournamentName}
-            placeholder="Tounrament Name"
+            placeholder="Tournament Name"
             required
             onChange={handleChange}
           />
@@ -67,8 +109,8 @@ export default function CreateTournament() {
             type="date"
             name="tournamentDate"
             value={formData.tournamentDate}
-            placeholder="Date of Match"
-           // required
+            placeholder="Date of Match (DD-MM-YYYY)"
+            required
             onChange={handleChange}
           />
 
@@ -77,8 +119,8 @@ export default function CreateTournament() {
             type="time"
             name="tournamentTime"
             value={formData.tournamentTime}
-            placeholder="IDP Time"
-            // required
+            placeholder="IDP Time (HH:mm)"
+            required
             onChange={handleChange}
           />
           <input
