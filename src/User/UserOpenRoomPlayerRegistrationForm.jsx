b601270031624a7b30877f20b2d93b3d0d0b3594
RahulK2396 <rahulvidx0696@gmail.com>
2023-10-25 15:58:19 +0530

    removed import statement

--- src/User/UserOpenRoomPlayerRegistrationForm.jsx
+++ src/User/UserOpenRoomPlayerRegistrationForm.jsx
@@ -6,7 +6,7 @@ import useFormatTime from "../hooks/useFormatTime";
 import useFormatDate from "../hooks/useFormatDate";
 import Footer from "../Footer/Footer";
 import { toast } from "react-toastify";
-import { newScrims } from "../api-Helpers/api-helpers";
+
 
 const UserOpenRoomPlayerRegisterForm = () => {
     const [teamdata, setTeamData] = useState({
