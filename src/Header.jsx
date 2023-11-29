25fac032057488a949df0a97c53990d974c9e2b1
kevin thomas <kevinthomas0420@gmail.com>
2023-11-29 14:36:27 +0530

    search bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,12 +8,23 @@ import ProfilePopUp from "./ProfilePopUp";
 import classes from "../src/ProfilePopUp.module.css";
 import store from "./Store";
 import { connect } from "react-redux";
-import notification  from "./assets/BellIcon.png"; 
+import notification from "./assets/BellIcon.png";
+import { allTournaments } from "./api-Helpers/api-helpers";
+
 function Header(props) {
   const navigate = useNavigate();
 
   const [isLogged, setIsLogged] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
+  const [tournaments, setTournaments] = useState();
+
+  useEffect(() => {
+    allTournaments()
+      .then((data) => setTournaments(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(tournaments);
 
   useEffect(() => {
     //console.log("Use effect",props.transfer);
@@ -35,10 +46,13 @@ function Header(props) {
     });
   }, [auth]);
   //console.log(isHovered);
-  const notify =()=> {
-    navigate("/NotificationList")
+  const notify = () => {
+    navigate("/NotificationList");
     console.log("notifyButton");
-  }
+  };
+
+  const [query, setQuery] = useState("");
+
   return (
     <div>
       <nav className="bg-black pb-[30px]">
@@ -46,8 +60,8 @@ function Header(props) {
           className="text-orange-500 font-semibold text-3xl cursor-pointer"
           onClick={() => {
             console.log("redux vlaue", props.transfer);
-          
-               navigate("/");
+
+            navigate("/");
           }}
         >
           EsBookify
@@ -57,22 +71,41 @@ function Header(props) {
             type="text"
             placeholder="search..."
             className="bg-transparent text-[#ff8a01] pl-[10px] w-[90%] placeholder:text-[#ff8a01] placeholder:pl-[2px]"
-          ></input>
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          {query && (
+            <ul className="text-black w-80 pt-6 z-50 mr-10">
+              {tournaments &&
+                tournaments
+                  .filter((tournament) =>
+                    tournament.name.toLowerCase().includes(query)
+                  )
+                  .map((tournament) => (
+                    <li
+                      onClick={() => navigate("/usertournaments")}
+                      key={tournament._id}
+                      className="bg-slate-100 mt-3 px-4"
+                    >
+                      {tournament.name}
+                    </li>
+                  ))}
+            </ul>
+          )}
           <a href="#">
             <FaMagnifyingGlass className="text-[#ff8a01] pt-[7px] text-xl" />
           </a>
         </div>
 
-            <div>
-            <button 
+        <div>
+          <button
             onClick={notify}
-              data-modal-target="defaultModal"
-              data-modal-toggle="defaultModal"
-              className={classes.profileButton}
-            >
-              <img  src={notification} alt="bellIcon"/> {" "}
-              </button>
-            </div>
+            data-modal-target="defaultModal"
+            data-modal-toggle="defaultModal"
+            className={classes.profileButton}
+          >
+            <img src={notification} alt="bellIcon" />{" "}
+          </button>
+        </div>
 
         <div className={classes.popupdiv} onMouseEnter={handleMouseEnter}>
           {isLogged && (
